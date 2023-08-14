235fcffd7f92bc90de724f1b03f26d2256d1796d
Luiz Fernando <bbatata686@gmail.com>
2023-08-14 14:30:07 -0300

    uma chamada de api para melhor desempenho

--- funcoes_genshin.py
+++ funcoes_genshin.py
@@ -1,16 +1,9 @@
 import genshin
 
-async def get_characters(ltuid : int, ltoken : str, uid: int):
+
+async def get_full_user_info(ltuid : int, ltoken : str, uid: int):
     cookies = {"ltuid": ltuid, "ltoken": ltoken}
     client = genshin.Client(cookies)
 
-    data = await client.get_genshin_characters(uid)
-    return data
-
-
-async def get_user_info(ltuid : int, ltoken : str, uid: int):
-    cookies = {"ltuid": ltuid, "ltoken": ltoken}
-    client = genshin.Client(cookies)
-
-    data = await client.get_genshin_user(uid)
+    data = await client.get_full_genshin_user(uid)
     return data
