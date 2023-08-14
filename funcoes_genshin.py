b4dea43f76dc8ac040b0e18d87d97b2b60b06191
Luiz Fernando <bbatata686@gmail.com>
2023-08-13 22:08:53 -0300

    adicionado função retorna informações de usuário

--- funcoes_genshin.py
+++ funcoes_genshin.py
@@ -6,3 +6,11 @@ async def get_characters(ltuid : int, ltoken : str, uid: int):
 
     data = await client.get_genshin_characters(uid)
     return data
+
+
+async def get_user_info(ltuid : int, ltoken : str, uid: int):
+    cookies = {"ltuid": ltuid, "ltoken": ltoken}
+    client = genshin.Client(cookies)
+
+    data = await client.get_genshin_user(uid)
+    return data
