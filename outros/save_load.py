4cef958e54eaa36e0c0d95c45ca7cdd1643b1b98
Luiz Fernando <bbatata686@gmail.com>
2023-10-31 14:34:01 -0300

    carregar padrao para usuarios

--- outros/save_load.py
+++ outros/save_load.py
@@ -1,10 +1,10 @@
 import json
 
-def salvar(conteudo, arquivo="saving"):
+def salvar(conteudo, arquivo="usuarios"):
     with open(arquivo + ".json", "w", encoding='UTF-8') as saving:
         json.dump(conteudo, saving)
 
-def carregar(arquivo="saving"):
+def carregar(arquivo="usuarios"):
     with open(arquivo + ".json", encoding='UTF-8') as loading:
         data = json.load(loading)
         return data
