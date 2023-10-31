5a09dcd7dc158916955054e482688133915f8080
Luiz Fernando <bbatata686@gmail.com>
2023-10-31 14:34:16 -0300

    adicionado método de checar usuario valido

--- outros/GenshinServico.py
+++ outros/GenshinServico.py
@@ -2,9 +2,16 @@ import genshin
 
 
 class GenshinServico():
-    async def get_genshin_user(ltuid : int, ltoken : str, uid: int):
-        cookies = {"ltuid": ltuid, "ltoken": ltoken}
+    async def get_genshin_user(cookies : dict, uid: int):
         client = genshin.Client(cookies)
 
         data = await client.get_genshin_user(uid)
         return data
+    
+    async def checar_usuario_valido(cookies : dict, uid : int):
+        try:
+            client = genshin.Client(cookies)
+            await client.get_genshin_user(uid)
+            return True
+        except:
+            return False
